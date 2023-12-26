67dff36f24db62845481201e4e4475fb82028b77
clay <dcheng69@uwo.ca>
2023-12-26 13:44:56 -0500

    Modify the boot.asm for fat12
    [Ticket: bootloader]
    
    [body]
    1. add code for fat12 file system boot sector
    
    [test]
    1. test manually

--- BootLoader/boot.asm
+++ BootLoader/boot.asm
@@ -2,7 +2,38 @@
 org 0x7c00
 
 ;===== this BaseOfStack provides intial value for the SP register
-BaseOfStack equ 0x7c00
+BaseOfStack             equ 0x7c00
+BaseOfLoader            equ 0x1000
+OffsetOfLoader          equ 0x00
+
+RootDirSectors          equ 14
+SectorNumOfRootDirStart equ 19
+SectorNumOfFAT1Start    equ 1
+SectorBalance           equ 17
+
+;===== Boot Sector of Fat12 Floppy
+jmp short Label_Start
+nop
+BS_OEMName          db      'TestBoot'
+BPB_BytesPerSec     dw      512
+BPB_SecPerClus      db      1
+BPB_RsvdSecCnt      dw      1
+BPB_NumFATs         db      2
+BPB_RootEntCnt      dw      224
+BPB_TotSec16        dw      2880
+BPB_Media           db      0xF0
+BPB_FATSz16         dw      9
+BPB_SecPerTrk       dw      18
+BPB_NumHeads        dw      2
+BPB_hiddSec         dd      0
+BPB_TotSec32        dd      0
+BPB_DrvNum          db      0
+BPB_Reserved1       db      0
+BS_BootSig          db      29h
+BS_VolID            dd      0
+BS_VolLab           db      'boot loader'
+BS_FileSysType      db      'FAT12   '
+;==== Boot Sector Occupy 62 bytes, the rest will be other code
 
 ;===== Program start point
 Label_Start:
@@ -44,7 +75,7 @@ Label_Start:
 
     jmp $
 
-StartBootMessage: db "clay boot program start!"
+StartBootMessage: db "Test boot program start!"
 
 ; ==== fill zero until whole sector
     times 510 - ($ - $$) db 0
